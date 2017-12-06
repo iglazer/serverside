1f23ca715c964f66ebed5df497823fb7b08687d3
iglazer <ian.glazer@gmail.com>
2017-12-06 17:08:04 -0500

    2

--- src/main/java/servlet/MainPage.java
+++ src/main/java/servlet/MainPage.java
@@ -58,24 +58,23 @@ public class MainPage extends HttpServlet{
         PrintWriter out = response.getWriter();
 
         String outputStr =  "<html><head>\n" +
-		   "<meta charset=\"utf-8\">
-		    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-		    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-		    <title>FIX, curated coffee components</title>";
-		outputStr  +="
-		    <link href="reset.css" rel="stylesheet">
-		    <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,600" type="text/css" rel="stylesheet">
-		    <link href="main.css" rel="stylesheet">"
-                "<meta name=\"salesforce-community\" content=\""+ communityUrl +"\">\n" +
+		   "<meta charset=\"utf-8\">" +
+		    "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" +
+		    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" +
+		    "<title>FIX, curated coffee components</title>";
+        
+		outputStr  += "<link href=\"reset.css\" rel=\"stylesheet\">\n" +
+		    "<link href=\"//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,600\" type=\"text/css\" rel=\"stylesheet\">\n" +
+		    "<link href=\"main.css\" rel=\"stylesheet\">" +
+                "<meta name=\"salesforce-community\" content=\""+ COMMUNITY +"\">\n" +
 //                "<meta name=\"salesforce-mode\" content=\""+ request.getParameter("mode") +"-callback\">\n" +
 				"<meta name=\"salesforce-mode\" content=\"" + System.getenv("SALESFORCE_MODE") + "-callback\">\n" +
                 "<meta name=\"salesforce-server-callback\" content=\"true\">\n" +
                 "<meta name=\"salesforce-server-response\" content='" + 
-                Base64.getEncoder().encodeToString(identityJSON.toString().getBytes(StandardCharsets.UTF_8))+"'>\n" +
                 "<meta name=\"salesforce-server-starturl\" content='" + startURL +"'>\n" +
                 "<meta name=\"salesforce-target\" content= \"#salesforce-login\">\n"+
                 "<meta name=\"salesforce-allowed-domains\" content=\"" + System.getenv("SALESFORCE_HEROKUAPP_URL") + "\">\n" +
-                "<script src=\""+ communityUrl +"/servlet/servlet.loginwidgetcontroller?type=javascript_widget\"" +
+                "<script src=\""+ COMMUNITY +"/servlet/servlet.loginwidgetcontroller?type=javascript_widget\"" +
                 " async defer></script>\n" +
                 "</head><body></body></html>";
         out.write(outputStr);
